73afd1c12e810c2dd860272d2843bdc344f68f99
Zirfaa <zirlydaffa321@gmail.com>
2026-01-17 12:00:14 +0700

    Add player gorund movement

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]private float MoveSpeed = 5f;
+    [SerializeField]private float jumpForce = 10f;
+    private bool isGrounded = false;
+    private float movement;
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(movement * MoveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
